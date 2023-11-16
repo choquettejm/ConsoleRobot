b154e36ea24880464e1a0f7b3a017aa9591043f7
choquettejm@outlook.com <choquettejm@outlook.com>
2023-11-16 15:13:44 -0500

    11162023 3:13pm updates

---
+++ ConsoleRobot/Robot.cs
@@ -0,0 +1,85 @@
+﻿
+
+
+
+
+namespace ConsoleRobot
+{
+
+
+    public class ActionResult
+    {
+        public string ActionName { get; set; }
+        public string AnimationName { get; set; }
+
+
+    }
+
+    public class YourRobot
+    {
+        public string robotName { get; set; }
+        public string[] studentNames { get; set; }
+        public double attack { get; set; }
+        public double defense { get; set; }
+        public double speed { get; set; }
+        public double constitution { get; set; }
+        public double health { get; set; }
+        public string primaryColor { get; set; }
+        public string secondaryColor { get; set; }
+
+
+        public YourRobot()
+        {
+
+            robotName = "test";
+            studentNames = new string[] { "Reece", "Jason" };
+            attack = 10;
+            defense = 10;
+            speed = 10;
+            constitution = 10;
+            health = 10 * constitution;
+            primaryColor = "";
+            secondaryColor = "";
+
+        }
+
+
+        public void Reset()
+        {
+            attack = default(double);
+            defense = default(double);
+            speed = default(double);
+            constitution = default(double);
+            health = GetMaxHeath();
+        }
+
+        public string GetAllStats()
+        {
+
+            return ($"Current Health: {health}, Attack: { attack}, Defense: { defense}, Speed: { speed}");
+
+        }
+
+        public double GetMaxHeath()
+        {
+            return 10 * constitution;
+        }
+
+        public void TakeDamage()
+        {
+            double damageDealt = 0;
+            double newHealth = 0;
+            double incomingDamage = 0;
+            double currentHealth = health;
+
+            damageDealt = incomingDamage - (defense / 100 * incomingDamage);
+            newHealth = currentHealth - damageDealt;
+
+
+        }
+
+
+    }
+}
+
+
---
+++ ConsoleRobot/RobotMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleRobot;
+
+
+    public class Program
+    {
+
+        public static void Main()
+        {
+
+            YourRobot robot = new YourRobot();
+        
+            Console.WriteLine(robot.GetMaxHeath());
+            Console.WriteLine(robot.GetAllStats());
+                
+        }
+
+
+
+
+
+    }
+
